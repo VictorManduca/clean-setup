9cb1339cf25fa487291085b3611c76a8a6f18b6e
VictorManduca <victor.manduca@hotmail.com>
2022-06-14 03:12:31 -0300

    chore: first commit

---
+++ src/helper.js
@@ -0,0 +1,46 @@
+import fs from "fs";
+import path from "path";
+import { promisify } from "util";
+
+import ncp from "ncp";
+
+const make = promisify(fs.mkdir)
+const access = promisify(fs.access);
+const copy = promisify(ncp);
+
+const currentFileUrl = import.meta.url;
+const templateDir = path.resolve(
+  new URL(currentFileUrl).pathname,
+  "../../templates/clean-arch-node"
+);
+
+async function copyTemplateFiles(options) {
+  return copy(templateDir, options.targetDirectory, {
+    clobber: false,
+  });
+}
+
+async function createProjectsFolder(options) {
+  return make(options.name)
+}
+
+export async function createProject(options) {
+  options = {
+    ...options,
+    targetDirectory: path.resolve(process.cwd(), options.name)
+  };
+
+  try {
+    await access(templateDir, fs.constants.R_OK);
+  } catch (err) {
+    console.error({ err });
+    console.error("%s Invalid template name");
+    process.exit(1);
+  }
+
+  await createProjectsFolder(options);
+  await copyTemplateFiles(options);
+
+  console.log("%s Project ready");
+  return true;
+}
---
+++ templates/clean-arch-node/src/main/server.ts
@@ -0,0 +1,5 @@
+import app from './config/app'
+import env from './config/env'
+
+app
+  .listen(env.PORT, () => console.log(`Server running at http://localhost:${env.PORT}`))
